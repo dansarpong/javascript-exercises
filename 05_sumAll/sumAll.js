3231c5934a61edc5c2db384acee00222fbcb2846
dansarpong <dansarpong.me@gmail.com>
2024-10-03 23:24:34 +0000

    Complete task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number' ||
+    a < 0 || b < 0 ||
+    a % 1 !== 0 || b % 1 !== 0
+  ) {
+    return 'ERROR';
+  }
+  let biggest = a>b ? a : b;
+  let smallest = biggest===a ? b : a;
+  let sum = 0;
 
+  for (;smallest <= biggest; smallest++) {
+    sum += smallest;
+  }
+  return sum;
 };
 
 // Do not edit below this line

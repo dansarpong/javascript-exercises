347349f4739b08c290e2e962a35a408c8e8f7395
dansarpong <dansarpong.me@gmail.com>
2024-10-03 14:45:38 +0000

    Fix repeating strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
 
+  let result = '';
+  if (times < 0) return 'ERROR';
+
+  while (times-- >= 1) {
+    result += word;
+  }
+
+  return result;
 };
 
 // Do not edit below this line

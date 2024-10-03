651e0e1a01ec3654635cdd4e472b43c6c8c3c313
dansarpong <dansarpong.me@gmail.com>
2024-10-03 22:58:54 +0000

    Complete task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+  let finalArray = array.filter((arrayElement) => {
+    for (const item of itemsToRemove) {
+      if (item === arrayElement) return false;
+    }
+    return true;
+  });
+
+  return finalArray;
 };
 
 // Do not edit below this line

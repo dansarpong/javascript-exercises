2b1d5088ead94af30f7b7b4ba75a1dabb182fc19
dansarpong <dansarpong.me@gmail.com>
2024-10-03 14:54:55 +0000

    Fix reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  let result = '';
+  while (word) {
+    result += word.slice(-1);
+    word = word.slice(0,-1);
+  }
 
+  return result;
 };
 
 // Do not edit below this line

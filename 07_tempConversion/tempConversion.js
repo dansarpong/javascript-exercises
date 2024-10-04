a67185e85e8d0e12bf1c465b47f70a25c3e7bc9a
dansarpong <dansarpong.me@gmail.com>
2024-10-04 00:32:46 +0000

    Complete task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let result = (degrees - 32) * (5/9);
+  return Math.round(result * 10) /10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let result = degrees * (9/5) + 32;
+  return Math.round(result * 10) /10;
 };
 
 // Do not edit below this line

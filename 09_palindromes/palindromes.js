b06b6edc2356e0178c1977862a3a54d527d0a9de
dansarpong <dansarpong.me@gmail.com>
2024-10-15 22:40:05 +0000

    Fix palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let cleanedString = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '').toLowerCase();
+  let reversedString = cleanedString.split("").reverse().join("");
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line

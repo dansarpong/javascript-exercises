fb6ed03f61afe1e4fa9a7638669865a68da6dba9
dansarpong <dansarpong.me@gmail.com>
2024-10-15 23:13:03 +0000

    Implement findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce(((oldest, person) => {
+    let age = (person.yearOfDeath) ?
+      person.yearOfDeath - person.yearOfBirth :
+      new Date().getFullYear() - person.yearOfBirth;
+    
+    if (age > oldest.age) {
+      oldest.person = person;
+      oldest.age = age;
+    }
 
+    return oldest;
+  }), {
+    person: null,
+    age: 0
+  }).person;
 };
 
 // Do not edit below this line
